e1b2691cbcdebf4780d0a824b4c082109b238d6c
asel <asel@lbfinance.com>
2026-02-26 16:21:23 +0530

    package.json

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 document.querySelector("#app").innerHTML = `
   <h1>Hello from Vite build!</h1>
-  <p>If you can download dist.zip from Actions, you did it ✅</p>
+  <p>npm run build should create dist/ ✅</p>
 `;
